08263f94b61dc7e1a4d770ddd556aec73493e008
Nisal Chandira De Zoysa <90953837+NisalDeZoysa@users.noreply.github.com>
2024-04-01 20:12:02 +0530

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Patient_final(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    NIC = models.SlugField()
+    age = models.IntegerField()
+    gender = models.IntegerField()
+    BMI = models.FloatField()
+    situation = models.FloatField()
+    seriousness = models.FloatField()
+    surgery_type = models.IntegerField()
+    time_slots = models.IntegerField()
+    surgeons = models.IntegerField()
+    urgancy = models.IntegerField()
+    category = models.IntegerField()
+    nature_of_surgery = models.IntegerField()
+    date_of_visit_day = models.IntegerField()
+    date_of_visit_month = models.IntegerField()
+    date_of_visit_year = models.IntegerField()
+    oparation_deadline_day = models.IntegerField()
+    oparation_deadline_month = models.IntegerField()
+    oparation_deadline_year = models.IntegerField()
+    veto_days_day = models.IntegerField(null=True)
+    veto_days_month = models.IntegerField(null=True)
+    veto_days_year = models.IntegerField(null=True)
+    surgery_time = models.FloatField()
+    surgery_date_day = models.IntegerField()
+    surgery_date_month = models.IntegerField()
+    surgery_date_year = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
